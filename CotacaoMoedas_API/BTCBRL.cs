bd0a2b2b174272e91301d8f90266ad7b1de67a81
LorayneNeves <lorayne3.8neves@outlook.com>
2023-06-10 22:09:57 -0300

    atualizando atividade

--- CotacaoMoedas_API/BTCBRL.cs
+++ CotacaoMoedas_API/BTCBRL.cs
@@ -6,14 +6,8 @@ using System.Threading.Tasks;
 
 namespace CotacaoMoedas_API
 {
-    // Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
-    public class Root
+    public class BTCBRL
     {
-        public int quantidade;
-        public string moeda1;
-        public string moeda2;
-        public string startDate;
-        public string endDate;
         public string code { get; set; }
         public string codein { get; set; }
         public string name { get; set; }
@@ -25,12 +19,5 @@ namespace CotacaoMoedas_API
         public string ask { get; set; }
         public string timestamp { get; set; }
         public string create_date { get; set; }
-        public USDBRL USDBRL { get; set; }
-        public EURBRL EURBRL { get; set; }
-        public BTCBRL BTCBRL { get; set; }
-
-
     }
-
-
 }
